bad2787fd82e277dcd5ae3666e54ebafcb3dfb36
Jaimy Kwa <j.t.s.kwa@students.uu.nl>
2022-11-24 19:28:44 +0100

    We got a proper camera now

--- Assignment1/camera.h
+++ Assignment1/camera.h
@@ -14,54 +14,54 @@ public:
 	Camera()
 	{
 		// setup a basic view frustum
-		camPos = float3( 0, 0, -2 );
-		topLeft = float3( -aspect, 1, 0 );
-		topRight = float3( aspect, 1, 0 );
-		bottomLeft = float3( -aspect, -1, 0 );
+		camPos_start = float3(0, 0, -2);
+		startDir = float3(0, 0, 1);
+		camPos = camPos_start;
+		topLeft_start = float3( -aspect, 1, 0 );
+		topRight_start = float3( aspect, 1, 0 );
+		bottomLeft_start = float3( -aspect, -1, 0 );
+		topLeft = topLeft_start;
+		topRight = topRight_start;
+		bottomLeft = bottomLeft_start;
 
 		auto dist_to_focus = length(camPos - ((topRight- bottomLeft) / 2)); // TODO: This should be the depth.
 
 	}
 
-	void move(float speed, float3 dircm_dir, bool zoom) {
-		//float3 dircm_dir = getdirection()* side;
-		//if(zoom ==false){ camPos += speed * dircm_dir; }
-		camPos += speed * dircm_dir;
-		topRight += speed * dircm_dir ;
-		bottomLeft += speed * dircm_dir ;
-		topLeft += speed * dircm_dir ;
+	void rotate(int2 mouse_movement, float speed) {
+		yaw += mouse_movement.x * speed;
+		pitch += mouse_movement.y * speed;
+		while (yaw > PI) yaw -= PI;
+		while (yaw < -PI) yaw += PI;
+		const float min_pitch = -(PI / 2) + FLT_MIN;
+		const float max_pitch = (PI / 2) - FLT_MIN;
+		pitch = clamp(pitch, min_pitch, max_pitch);
+		topLeft =  mat4::RotateY(-yaw) * (mat4::RotateX(-pitch) * (topLeft_start - camPos_start));
+		topRight = mat4::RotateY(-yaw) * (mat4::RotateX(-pitch) * (topRight_start - camPos_start));
+		bottomLeft = mat4::RotateY(-yaw) * (mat4::RotateX(-pitch) * (bottomLeft_start - camPos_start));
+		topRight += camPos;
+		bottomLeft += camPos;
+		topLeft += camPos;
 	}
+
+	void move(float3 dir, float speed) {
+		// Movement in camera space
+		float3 move = mat4::RotateY(-yaw) * (mat4::RotateX(-pitch) * dir);
+		camPos += move * speed;
+		topLeft += move * speed;
+		topRight += move * speed;
+		bottomLeft += move * speed;
+	}
+
 	float3 getdirection() {
 		return normalize((topRight + bottomLeft)/2 - camPos );
 	}
 
-	void zoom(float speed) {
-		float3 cam_dir = getdirection();
-		topLeft += speed * cam_dir; topRight += speed * cam_dir; bottomLeft += speed * cam_dir;
-	}
-	void rotate_camAxis(mat4 TMatrix)
-	{
-			auto ss = topLeft - camPos;
-			topLeft =  ((float3)(TMatrix * (topLeft - camPos)) + camPos);
-			topRight =  ((float3)(TMatrix * (topRight - camPos)) + camPos);
-			bottomLeft = ((float3)(TMatrix * (bottomLeft - camPos)) + camPos);
-	}
+	//void zoom(float speed) {
+	//	float3 cam_dir = getdirection();
+	//	topLeft += speed * cam_dir; topRight += speed * cam_dir; bottomLeft += speed * cam_dir;
+	//}
 
-	void rotate_cam(float3 rot)
-	{
-		if (rot.x + rot.y + rot.z != 0)
-		{
-			mat4 TMatrix = mat4::Rotate(normalize(rot), length(rot));
-			auto ss = topLeft - camPos;
-			topLeft = (float3)(TMatrix * (topLeft - camPos)) + camPos;
-			topRight = (float3)(TMatrix * (topRight - camPos)) + camPos;
-			bottomLeft = (float3)(TMatrix * (bottomLeft - camPos)) + camPos;
-		}
-	}
-
-	inline double degrees_to_radians(double degrees) {
-		return degrees * PI / 180.0;
-	}
 
 	Ray GetPrimaryRay( const int x, const int y )
 	{
@@ -73,8 +73,12 @@ public:
 	}
 	float aspect = (float)SCRWIDTH / (float)SCRHEIGHT;
 	float ZoomLevel = 1.0f;
-	float3 camPos;
+	float3 camPos, camPos_start, startDir;
+	float yaw = 0, pitch = 0;
+	mat4 cam_matrix;
+	float3 topLeft_start, topRight_start, bottomLeft_start;
 	float3 topLeft, topRight, bottomLeft;
+
 	
 
 
