9978556b4122d8322952e611d75f469c67c272d0
Jaimy Kwa <j.t.s.kwa@students.uu.nl>
2022-12-21 22:18:37 +0100

    Forgot scenegpu.h

--- Assignment1/scenegpu.h
+++ Assignment1/scenegpu.h
@@ -12,10 +12,18 @@ namespace Tmpl8 {
 		cl_uint id;
 	};
 
+	__declspec(align(64)) struct MaterialGPU {
+		cl_float4 albedoSpecularity;
+		cl_float4 absorption;
+		cl_bool isEmissive;
+		cl_int textureId;
+	};
+
 	class SceneGPU {
 	public:
 		SceneGPU() {
-			tris = new TriGPU[2];
+			tris = new TriGPU[4];
+			mats = new MaterialGPU[4];
 			// First test triangle
 			float3 v0_i = float3(3.f, -3.f, 3.f);
 			float3 v1_i = float3(3.f, 3.f, 3.f);
@@ -35,8 +43,8 @@ namespace Tmpl8 {
 			float3 v0_j = float3(3.f, 3.f, -3.f);
 			float3 v1_j = float3(3.f, -3.f, -3.f);
 			float3 v2_j = float3(3.f, 3.f, 3.f);
-			float3 c_j = (v0_i + v1_i + v2_i) / 3.f;
-			float3 N_j = normalize(cross((v1_i - v0_i), (v2_i - v0_i)));
+			float3 c_j = (v0_j + v1_j + v2_j) / 3.f;
+			float3 N_j = normalize(cross((v1_j - v0_j), (v2_j - v0_j)));
 			tris[1] = {
 				{v0_j.x, v0_j.y, v0_j.z, c_j.x},
 				{v1_j.x, v1_j.y, v1_j.z, c_j.y},
@@ -46,11 +54,60 @@ namespace Tmpl8 {
 				N_j.z,
 				1,
 			};
-			triColors = new cl_float4[2];
-			triColors[0] = { 1.f, 0.f, 0.f, 1.f };
-			triColors[1] = { 0.f, 1.f, 0.f, 1.f };
+			// Lamp in the air
+			float3 v0_k = float3(1.5f, 2.95f, 1.5f);
+			float3 v1_k = float3(-1.5f, 2.95f, 1.5f);
+			float3 v2_k = float3(1.5f, 2.95f, -1.5f);
+			float3 c_k = (v0_k + v1_k + v2_k) / 3.f;
+			float3 N_k = normalize(cross((v1_k - v0_k), (v2_k - v0_k)));
+			tris[2] = {
+				{v0_k.x, v0_k.y, v0_k.z, c_k.x},
+				{v1_k.x, v1_k.y, v1_k.z, c_k.y},
+				{v2_k.x, v2_k.y, v2_k.z, c_k.z},
+				N_k.x,
+				N_k.y,
+				N_k.z,
+				2,
+			};
+			float3 v0_l = float3(-1.5f, 2.95f, -1.5f);
+			float3 v1_l = float3(1.5f, 2.95f, -1.5f);
+			float3 v2_l = float3(-1.5f, 2.95f, 1.5f);
+			float3 c_l = (v0_l + v1_l + v2_l) / 3.f;
+			float3 N_l = normalize(cross((v1_l - v0_l), (v2_l - v0_l)));
+			tris[3] = {
+				{v0_l.x, v0_l.y, v0_l.z, c_l.x},
+				{v1_l.x, v1_l.y, v1_l.z, c_l.y},
+				{v2_l.x, v2_l.y, v2_l.z, c_l.z},
+				N_l.x,
+				N_l.y,
+				N_l.z,
+				3,
+			};
+			MaterialGPU mat0 = {
+				{1, 0, 0, 0},
+				{0, 0, 0, 0},
+				false,
+				-1,
+			};
+			MaterialGPU mat1 = {
+				{0, 1, 0, 0},
+				{0, 0, 0, 0},
+				false,
+				-1,
+			};
+			MaterialGPU mat2 = {
+				{24, 24, 24, 0},
+				{0, 0, 0, 0},
+				true,
+				-1,
+			};
+			mats[0] = mat0;
+			mats[1] = mat1;
+			mats[2] = mat2;
+			mats[3] = mat2;
 		}
 		TriGPU* tris;
-		cl_float4* triColors;
+		MaterialGPU* mats;
+		int tri_count = 4;
 	};
 }
